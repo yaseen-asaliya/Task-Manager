0489bed21436c02a5bd01466673f5831b5da0d76
Yaseen asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2022-07-10 20:56:10 +0300

    Prototype

---
+++ src/main/java/com/training/TaskManger/TaskMangerApplication.java
@@ -0,0 +1,13 @@
+package com.training.TaskManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/TaskManger/TaskMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.TaskManger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
