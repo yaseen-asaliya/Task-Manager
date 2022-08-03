94b0dda9304ac68bb9719c92d9eda48cf28101e5
Yaseen asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2022-08-03 17:02:58 +0300

    extract tasks function

--- src/main/java/com/training/taskmanger/service/TaskServiceImplementation.java
+++ src/main/java/com/training/taskmanger/service/TaskServiceImplementation.java
@@ -1,15 +1,16 @@
 package com.training.taskmanger.service;
 
 import com.training.taskmanger.entity.Task;
+import com.training.taskmanger.entity.User;
 import com.training.taskmanger.repository.TaskRepository;
 import com.training.taskmanger.exception.NotFoundException;
+import com.training.taskmanger.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class TaskServiceImplementation implements Services<Task> {
@@ -18,12 +19,15 @@ public class TaskServiceImplementation implements Services<Task> {
     private TaskRepository taskRepository;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     public TaskServiceImplementation(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
-    public List<Object> getTasks(int userId){
-        return taskRepository.findTasksByUserId(userId);
+    public List<Task> getTasks(int userId){
+        return extractTasks(userId);
     }
 
     @Override
@@ -50,5 +54,27 @@ public class TaskServiceImplementation implements Services<Task> {
     @Override
     public void deleteById(int taskId) {
         taskRepository.deleteById(taskId);
+    }
+
+    private List<Task> extractTasks(int userId){
+        List<Object> list = taskRepository.findTasksByUserId(userId);
+        List<Task> tasks = new ArrayList<>();
+        for (int i=0; i<list.size(); i++){
+            Object[] row = (Object[]) list.get(i);
+            Optional<User> user = userRepository.findById(userId);
+            if (user == null) {
+                throw new NotFoundException("User not found");
+            }
+            Task task = new Task((int) row[0],
+                    user.get(),
+                    (String) row[1],
+                    (int) row[2],
+                    (String) row[3],
+                    (String) row[4]);
+            System.out.println(task);
+            tasks.add(task);
+        }
+        return tasks;
+
     }
 }
