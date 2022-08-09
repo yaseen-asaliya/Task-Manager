de129712e5cdc76002bed95c31f644ed7c78a7d2
Yaseen asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2022-08-09 12:00:29 +0300

    Authentication controller test cases

--- src/test/java/com/training/taskmanger/controllers/AuthenticationTests.java
+++ src/test/java/com/training/taskmanger/controllers/AuthenticationTests.java
@@ -1,35 +1,49 @@
 package com.training.taskmanger.controllers;
 
+import com.training.taskmanger.security.http.request.LoginRequest;
+import com.training.taskmanger.security.http.request.SignupRequest;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class AuthenticationTests {
 
 	@Mock
-	@Autowired
 	private AuthenticationController authenticationController;
 
 	@Test
 	void should_signin() {
-
+		LoginRequest request = new LoginRequest("ys","123");
+		ResponseEntity response = new ResponseEntity(HttpStatus.ACCEPTED);
+		when(authenticationController.authenticateUser(request)).thenReturn(response);
+		assertEquals(response,authenticationController.authenticateUser(request));
 	}
 
 	@Test
 	void should_signup() {
-
+		SignupRequest request = new SignupRequest("yaseen","123","yaseen@test.com","ys");
+		ResponseEntity response = new ResponseEntity(HttpStatus.ACCEPTED);
+		when(authenticationController.registerUser(request)).thenReturn(response);
+		assertEquals(response,authenticationController.registerUser(request));
 	}
-
 
 	@Test
 	void should_Logout() {
-
+		ResponseEntity response = new ResponseEntity(HttpStatus.ACCEPTED);
+		when(authenticationController.logoutUser()).thenReturn(response);
+		assertEquals(response,authenticationController.logoutUser());
 	}
 
 	@Test
 	void should_logout_from_all_places() {
-
+		ResponseEntity response = new ResponseEntity(HttpStatus.ACCEPTED);
+		when(authenticationController.logoutAll()).thenReturn(response);
+		assertEquals(response,authenticationController.logoutAll());
 	}
 }
