c91fe0bd8ce38fed85ff798c31c61f347efacd80
Yaseen asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2022-09-10 13:05:33 +0300

    Refactor time conflict class

--- src/main/java/com/training/taskmanger/exception/RestExceptionHandler.java
+++ src/main/java/com/training/taskmanger/exception/RestExceptionHandler.java
@@ -4,11 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-public class RestExceptionHandler<E> {
+@RestControllerAdvice
+public class RestExceptionHandler {
 
     public final Logger LOGGER = LoggerFactory.getLogger(RestExceptionHandler.class.getName());
 
